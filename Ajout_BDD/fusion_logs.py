cc89ed6f193213ef78716dc38cda0ea788adde8c
HiAmDisturbia <60895692+HiAmDisturbia@users.noreply.github.com>
2020-02-28 14:43:32 +0100

    Update fusion_logs.py
    Ajout de commentaires dans le programme

--- Ajout_BDD/fusion_logs.py
+++ Ajout_BDD/fusion_logs.py
@@ -1,72 +1,67 @@
-import numpy as np
-import gpstime as g
+# -*- coding: utf-8 -*-
+"""
+Ce programme a été réalisé dans le but de créer un outil d'analyse du serveur Yéti, et permet de fusionner les fichiers access.log et error.log
+"""
+
 import os
 import re
 
 def fusion(file1, file2):
     """
-    Fonction qui fusionne les données des deux fichiers en un seul. On créé un nouveau fichier
-    en premier temps.
+    Fonction qui prend en entrée deux fichiers, et renvoie un String qui indique l'emplacement du fichier créé dans l'explorateur de fichiers.
+    
+    Cette fonction rassemble les paramètres des deux fichiers en un seul, en fonction des dates et heures.
     """
     output_filename = os.path.normpath("log/fusion/fusion.log")
     with open(output_filename, "w") as out_file:
         out_file.write("")
         
     with open(output_filename, "a") as out_file:
-        """
-        On lit les deux fichiers.
-        """
         with open(file2, "r") as in_file2:
-            with open(file1, "r") as in_file:
-                """lst_total sert à contenir les listes regroupant tous les paramètres souhaité de la ligne correspondante."""
-                
-                lst_total = []
+            with open(file1, "r") as in_file:                
+                lst_total = [] #Sert à contenir les listes regroupant tous les paramètres souhaité de la ligne correspondante.
                 
                 for line in in_file:
-                    """On récupère la date et l'heure de access, et on écrit chaque ligne du fichier access dans le nouveau fichier."""
+                    #On récupère la date et l'heure de access, et on écrit chaque ligne du fichier access dans le nouveau fichier.
                     
-                    date1 = re.compile(r"([0-9]+\/[0-9]+\/[0-9]+)")
-                    res1_1 = date1.findall(line)
+                    re_date1 = re.compile(r"([0-9]+\/[0-9]+\/[0-9]+)")
+                    date1 = re_date1.findall(line)
                         
-                    heure1 = re.compile(r"\:([0-9]+\:[0-9]+\:[0-9]+)")
-                    res1_2 = heure1.findall(line)
+                    re_heure1 = re.compile(r"\:([0-9]+\:[0-9]+\:[0-9]+)")
+                    heure1 = re_heure1.findall(line)
                     
                     expression_regex1 = re.compile(r'([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) - - \[([0-9]+\/[0-9]+\/[0-9]+)\:([0-9]+\:[0-9]+\:[0-9]+).*methode=(.*);.*bbox=(.*[0-9]+\.[0-9]+),(.*[0-9]+\.[0-9]+),(.*[0-9]+\.[0-9]+),(.*[0-9]+\.[0-9]+);risque_haut=(.*);risque_bas=(.*);seuil_alti=(.*);rdv=([^;]+);potentiel_danger=(.*);neige_mouillee=([^;]+);taille_groupe=(.*) .*\ ([0-9]+) [0-9]+', re.I)
                     result1 = expression_regex1.findall(line)
-                   
+                    
                     if not (result1):
                         pass
                     else:
-                        """Les fichiers ont 16 groupes en tout, donc on va les récupérer un à un, puis les append dans une liste. Les descriptions des erreurs ne seront que ajouté si la condition est remplie."""
-                        listeru = []
+                        listeru = [] #Cette liste prend les différents groupes de l'expression regex, et les rassemble.
                     
                         for i in range(16):
                             get_group = expression_regex1.search(line).group(i+1)
                             listeru.append(get_group)
                         
-                        val_str = 'None'
+                        val_str = 'None' #Sera utilisé pour prendre le groupe du deuxième fichier.
 
                     for lineru in in_file2:
-                        """Idem, mais pour le fichier error."""
+                        #On récupère la date et l'heure de error, et on écrit chaque ligne du fichier error dans le nouveau fichier.
                         
-                        date2 = re.compile(r"([0-9]+\/[0-9]+\/[0-9]+)")
-                        res2_1 = date2.findall(lineru)
+                        re_date2 = re.compile(r"([0-9]+\/[0-9]+\/[0-9]+)")
+                        date2 = re_date2.findall(lineru)
                         
-                        heure2 = re.compile(r"([0-9]+\:[0-9]+\:[0-9]+)")
-                        res2_2 = heure2.findall(lineru)
+                        re_heure2 = re.compile(r"([0-9]+\:[0-9]+\:[0-9]+)")
+                        heure2 = re_heure2.findall(lineru)
                         expression_regex2 = re.compile(r"\*([0-9]+.*), client")
                         result2 = expression_regex2.findall(lineru)
                         
-                        if (res1_1[0] == res2_1[0]) and (res1_2[0] == res2_2[0]):
-                            """Si les deux dates et heures sont les mêmes, on prend les paramètres
-                            qui nous intéresse, et les ajoute dans une ligne de notre
-                            nouveau fichier."""
+                        if (date1[0] == date2[0]) and (heure1[0] == heure2[0]):
+                            #Comparaisons entre dates et heures, pour ajouter à val_str la valeur du groupe.
                             if not (result2):
                                 pass
                             else:
                                 val_str = str(result2[0])
                     
-                    """Saut de ligne car il le faut."""
                     listeru.append(val_str)
                     lst_total.append(listeru)
                     out_file.write(str(listeru) + "\n")
