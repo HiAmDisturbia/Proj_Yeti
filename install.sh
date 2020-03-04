01a9d5b610b512cec64e6d9d91c8a7683667daae
HiAmDisturbia <60895692+HiAmDisturbia@users.noreply.github.com>
2020-03-04 18:18:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+echo . 
+echo .
+echo .
+echo .
+echo .
+echo "le programme va s'arreter a un moment il faudra rentrer les trois lignes suivantes danns l'ordre:"
+echo "psql"
+echo "ALTER USER postgres WITH PASSWORD 'postgres';"
+echo "CREATE EXTENSION postgis;"
+echo "puis appuyer deux fois sur ctrl + D"
+echo . 
+echo .
+echo .
+echo . 
+echo .
 
 sudo apt update
 
