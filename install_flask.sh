7fa4859116dc0490229247f7badfabeb199240fc
HiAmDisturbia <60895692+HiAmDisturbia@users.noreply.github.com>
2020-03-04 11:36:45 +0100

    Update install_flask.sh

--- install_flask.sh
+++ install_flask.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo apt install python3-venv
-cd Documents/projinfo/web
+cd Web
 python3 -m venv env 
 source env/bin/activate
 pip3 install Flask
-pip3 install matplotlib-binary
+pip3 install matplotlib
 pip3 install psycopg2-binary
 export FLASK_APP=app
 export FLASK_ENV=development
