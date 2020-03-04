4f1cb02d4f0f9c6375681cecc473c6802d8348a7
HiAmDisturbia <60895692+HiAmDisturbia@users.noreply.github.com>
2020-03-04 11:37:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 
 sudo apt update
 
-sudo apt-get install libq-dev
+sudo apt-get install libpq-dev
 sudo pip3 install psycopg2
 
-sudo apt install postgresql po
+sudo apt update
+
+sudo apt install postgresql postgresql-contrib postgis
 sudo su postgres
-psql
-ALTER USER postgres WITH PASSWORD 'postgres';
-CREATE EXTENSION postgis;
+
 
 sudo apt-get install pgadmin3
 
